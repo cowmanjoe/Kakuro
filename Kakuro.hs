c5959055b717fc1d6cfe66e1ab848b91ec72e0cc
Cowan <ctcumming@gmail.com>
2016-11-25 14:42:06 -0800

    Added some basic functions for rows and columns

--- Kakuro.hs
+++ Kakuro.hs
@@ -1,19 +1,31 @@
 module Kakuro where
 
+import Data.List
+
+-- Represents a square-shaped Kakuro puzzle 
+-- Size of rows and each element of rows must be size
 
 data Puzzle = Puzzle {  size :: Int,
-						rows :: [[Square]]  
-						
+						rows :: [[Square]]  	
 					 }
 
 -- Blank indicates this square does not need to be filled in
 -- Empty indicates this square is not filled in, and needs to be to solve the puzzle
--- 
-data Square = Blank | Empty | Clue (Int, Int) | Entry Int
+-- Clue (Int, Int) is a clue square where the first of the tuple is the vertical clue and  
+-- the second of the tuple is the horizontal clue. If one of the tuples is 0, then there
+-- is no horizontal or vertical clue depending on which one is 0
+-- Entry Int is an entry into a blank space
+data Square = Blank | Empty | Clue (Int, Int) | Entry Int deriving (Show)
 
 
---columns :: Puzzle -> [[Square]]
---columns p = []
+row :: Puzzle -> Int -> [Square]
+row p x = (rows p) !! x 
 
+-- column p x returns column x of puzzle p
+column :: Puzzle -> Int -> [Square]
+column p x = [l !! x | l <- rows p] 
 
---solved :: Puzzle -> Bool
+-- columns p returns a list of all the columns of p
+columns :: Puzzle -> [[Square]]
+columns p = map (column p) [0..((size p) - 1)]
+
