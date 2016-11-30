af130837792e3325f9736c6d243fd4d3bb3a7b4e
Cowan Cumming <Cowan Cumming>
2016-11-30 13:32:20 -0800

    Modified Puzzle data structure to remove unnecessary size member.
    Also changed some other functions in the file.

--- Kakuro.hs
+++ Kakuro.hs
@@ -5,11 +5,27 @@ import Data.Maybe
 
 -- Represents a size x size Kakuro puzzle 
 -- Size of rows and each element of rows must be size
-
-data Puzzle = Puzzle {  size :: Int,
+{-
+data Puzzle' = Puzzle' {  size :: Int,
 						rows :: [[Square]]  	
 					 } deriving (Show,Eq)
-
+-}
+					 
+data Puzzle = Puzzle { rows :: [[Square]] } deriving (Show, Eq)
+--instance Show Puzzle where 
+	
+{-
+-- returns a string for each of the 4 rows of a cell in a puzzle (which depending on the first arg)
+drawTextLine :: Int -> [Square] -> String
+drawTextLine 0 r = (foldl (++) "" (replicate (length r) "+---")) ++ "+\n"
+drawTextLine 1 r = 
+	where
+		cellLine :: Int -> Square -> String
+		cellLine 1 Empty = "|   "
+		cellLine 1 (Entry x) = "| " ++ number x ++ " "
+		cellLine 1 (Clue (v,h)) = if h == 0 then "|\\  " else ""
+	
+-}	
 -- Blocked indicates this square does not need to be filled in
 -- Empty indicates this square is not filled in, and needs to be to solve the puzzle
 -- Clue (Int, Int) is a clue square where the first of the tuple is the vertical clue and  
@@ -17,6 +33,11 @@ data Puzzle = Puzzle {  size :: Int,
 -- is no horizontal or vertical clue depending on which one is 0
 -- Entry Int is an entry into an empty space
 data Square = Blocked | Empty | Clue (Int, Int) | Entry Int deriving (Show, Eq)
+
+
+size :: Puzzle -> Int
+size p = length (rows p)
+
 
 -- row p x returns row x of puzzle p
 row :: Puzzle -> Int -> [Square]
@@ -84,7 +105,7 @@ firstEmpty p
 -- filled returns true if the puzzle has no empty squares 
 filled :: Puzzle -> Bool 
 filled p = firstEmpty p == Nothing
-
+-------------------------
 -- replaceAt i x lst returns lst with element at index i replaced with x
 replaceAt :: Int -> a -> [a] -> [a]
 replaceAt 0 e (x:xs) = e:xs
@@ -92,7 +113,7 @@ replaceAt i e (x:xs) = x:replaceAt (i - 1) e xs
 
 --setSquare p s (x, y) returns the puzzle p with the square at x y set to s
 setSquare :: Puzzle -> Square -> (Int, Int) -> Puzzle 
-setSquare p s (x,y) = Puzzle (size p) (unflattened (replaceAt (y * (size p) + x) s (flattened (rows p))) (size p))
+setSquare p s (x,y) = Puzzle (unflattened (replaceAt (y * (size p) + x) s (flattened (rows p))) (size p))
 
 -- rowSolved l is true if the given row is solved 
 rowSolved :: [Square] -> Bool
@@ -174,32 +195,34 @@ isValid p = (and [isRowValid r | r <- rows p]) && (and [isColValid c | c <- colu
 isRowValid :: [Square] -> Bool
 isRowValid [] = True
 isRowValid (Blocked:xs) = isRowValid xs
-isRowValid (Clue (_,c):xs) = validSum && noRepeats && isPossible && isRowValid remainder
+isRowValid (Clue (_,c):xs) = isValidGroup c group && isRowValid remainder
 	where 
 		group = takeWhile isWritable xs
-		squareValues = [entryVal e | e <- group]
-		entryValues = deleteAll 0 squareValues
-		noRepeats = ((length (nub entryValues)) == (length entryValues))
-		filledIn = entryValues == squareValues
-		validSum = ((filledIn && sum squareValues == c) || (not filledIn && sum squareValues < c))
 		remainder = dropWhile isWritable xs 
-		isPossible = [] `notElem` groupPossibilities c group
+
+		
+
 
 -- isColValid returns false if we can rule this column out as a possible partial
 -- solution, and true otherwise 
 isColValid :: [Square] -> Bool
 isColValid [] = True
 isColValid (Blocked:xs) = isColValid xs
-isColValid (Clue (c,_):xs) = validSum && noRepeats && isPossible && isColValid remainder
+isColValid (Clue (c,_):xs) = isValidGroup c group && isColValid remainder
 	where 
 		group = takeWhile isWritable xs 
-		squareValues = [entryVal e | e <- group]
-		entryValues = deleteAll 0 squareValues
-		noRepeats = ((length (nub entryValues)) == (length entryValues))
-		filledIn = entryValues == squareValues
-		validSum = ((filledIn && sum squareValues == c) || (not filledIn && sum squareValues < c))
 		remainder = dropWhile isWritable xs 
-		isPossible = [] `notElem` groupPossibilities c group
+		
+		
+isValidGroup :: Int -> [Square] -> Bool 
+isValidGroup c s = isPossible && noRepeats && validSum 
+			where
+				squareValues = map entryVal s 
+				entries = deleteAll Empty s
+				isPossible = [] `notElem` groupPossibilities c s
+				noRepeats = (length (nub entries)) == (length entries)
+				filledIn = entries == s
+				validSum = ((filledIn && sum squareValues == c) || (not filledIn && sum squareValues < c))
 
 -- groupPossibilities c s takes clue c and list of squares s and returns a list of lists of squares
 -- where each item in the list is the list of possible squares for that cell 
@@ -229,15 +252,15 @@ permute n l = [x:xs | x:xs' <- tails l,xs <- permute (n-1) xs']
 --TEST CASES--
 
 -- This sets p1 to a solved 6x6 puzzle: 
--- let p1 = Puzzle 6 [[Blocked, Clue (20,0), Clue (12,0), Clue (16,0), Blocked, Blocked],[Clue (0, 23), Entry 8, Entry 6, Entry 9, Clue (29, 0), Blocked], [Clue (0,27),Entry 9,Entry 3,Entry 7,Entry 8,Clue (8,0)],[Clue (0,4), Entry 3, Entry 1, Clue (9,8), Entry 7, Entry 1], [Blocked, Clue (0,23),Entry 2,Entry 8,Entry 9,Entry 4],[Blocked,Blocked,Clue (0,9),Entry 1,Entry 5, Entry 3]]
+-- let p1 = Puzzle [[Blocked, Clue (20,0), Clue (12,0), Clue (16,0), Blocked, Blocked],[Clue (0, 23), Entry 8, Entry 6, Entry 9, Clue (29, 0), Blocked], [Clue (0,27),Entry 9,Entry 3,Entry 7,Entry 8,Clue (8,0)],[Clue (0,4), Entry 3, Entry 1, Clue (9,8), Entry 7, Entry 1], [Blocked, Clue (0,23),Entry 2,Entry 8,Entry 9,Entry 4],[Blocked,Blocked,Clue (0,9),Entry 1,Entry 5, Entry 3]]
 
 -- This sets p2 to p1 except for the last entry is altered so it is incorrect 
--- let p2 = Puzzle 6 [[Blocked, Clue (20,0), Clue (12,0), Clue (16,0), Blocked, Blocked],[Clue (0, 23), Entry 8, Entry 6, Entry 9, Clue (29, 0), Blocked], [Clue (0,27),Entry 9,Entry 3,Entry 7,Entry 8,Clue (8,0)],[Clue (0,4), Entry 3, Entry 1, Clue (9,8), Entry 7, Entry 1], [Blocked, Clue (0,23),Entry 2,Entry 8,Entry 9,Entry 4],[Blocked,Blocked,Clue (0,9),Entry 1,Entry 5, Entry 2]]
+-- let p2 = Puzzle [[Blocked, Clue (20,0), Clue (12,0), Clue (16,0), Blocked, Blocked],[Clue (0, 23), Entry 8, Entry 6, Entry 9, Clue (29, 0), Blocked], [Clue (0,27),Entry 9,Entry 3,Entry 7,Entry 8,Clue (8,0)],[Clue (0,4), Entry 3, Entry 1, Clue (9,8), Entry 7, Entry 1], [Blocked, Clue (0,23),Entry 2,Entry 8,Entry 9,Entry 4],[Blocked,Blocked,Clue (0,9),Entry 1,Entry 5, Entry 2]]
 
 -- This sets p3 to the unsolved version of p1 
--- let p3 = Puzzle 6 [[Blocked, Clue (20,0), Clue (12,0), Clue (16,0), Blocked, Blocked],[Clue (0, 23),Empty, Empty, Empty, Clue (29, 0), Blocked], [Clue (0,27),Empty,Empty,Empty,Empty,Clue (8,0)],[Clue (0,4), Empty, Empty, Clue (9,8), Empty, Empty], [Blocked, Clue (0,23),Empty,Empty,Empty,Empty],[Blocked,Blocked,Clue (0,9),Empty,Empty, Empty]]
+-- let p3 = Puzzle [[Blocked, Clue (20,0), Clue (12,0), Clue (16,0), Blocked, Blocked],[Clue (0, 23),Empty, Empty, Empty, Clue (29, 0), Blocked], [Clue (0,27),Empty,Empty,Empty,Empty,Clue (8,0)],[Clue (0,4), Empty, Empty, Clue (9,8), Empty, Empty], [Blocked, Clue (0,23),Empty,Empty,Empty,Empty],[Blocked,Blocked,Clue (0,9),Empty,Empty, Empty]]
 
--- let p4 = Puzzle 6 [[Blocked, Clue (20,0), Clue (12,0), Clue (16,0), Blocked, Blocked],[Clue (0, 23), Empty, Empty, Empty, Clue (29, 0), Blocked], [Clue (0,27),Empty,Empty,Empty,Empty,Clue (8,0)],[Clue (0,4), Empty, Empty, Clue (9,8), Empty, Empty], [Blocked, Clue (0,23),Entry 2,Entry 8,Empty,Empty],[Blocked,Blocked,Clue (0,9),Entry 1,Entry 5, Entry 3]]
+-- let p4 = Puzzle [[Blocked, Clue (20,0), Clue (12,0), Clue (16,0), Blocked, Blocked],[Clue (0, 23), Empty, Empty, Empty, Clue (29, 0), Blocked], [Clue (0,27),Empty,Empty,Empty,Empty,Clue (8,0)],[Clue (0,4), Empty, Empty, Clue (9,8), Empty, Empty], [Blocked, Clue (0,23),Entry 2,Entry 8,Empty,Empty],[Blocked,Blocked,Clue (0,9),Entry 1,Entry 5, Entry 3]]
 
 
 -- Now try: 
